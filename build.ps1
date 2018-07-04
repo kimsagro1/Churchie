fd7bf391d4117e2163320076251b95a3157fe20a
kimsagro1 <kimsagro@gmail.com>
2018-07-04 13:17:04 +1000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,23 +1,25 @@
 Param
 (
     [String]$CakeVersion = "0.28.1",
-    [String]$ToolsDir    = "$PSScriptRoot/tools",
-    [String]$BuildFile   = "$PSScriptRoot/build.cake",
-    [String]$ToolsProj   = "$ToolsDir/tools.csproj",
-    [String]$Target      = 'Default',
-    [String]$Verbosity   = 'Verbose'
+    [String]$ToolsDir    = [io.path]::combine($PSScriptRoot, "tools"),
+    [String]$BuildFile   = [io.path]::combine($PSScriptRoot, "build.cake"),
+    [String]$ToolsProj   = [io.path]::combine($ToolsDir, "tools.csproj"),
+    [String]$Target      = "Default",
+    [String]$Verbosity   = "Verbose"
 )
 
-$CAKE_DIR = "$ToolsDir/Cake.CoreCLR.$CakeVersion"
-$CAKE_DLL = "$CAKE_DIR/cake.coreclr/$CakeVersion/Cake.dll"
+$CAKE_DIR = [io.path]::combine($ToolsDir, "Cake.CoreCLR.$CakeVersion")
+$CAKE_DLL = [io.path]::combine($CAKE_DIR, "cake.coreclr", "$CakeVersion", "Cake.dll")
 
 if (-not (Test-Path $ToolsProj))
 {
     $projectFileContents = '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp2.0</TargetFramework></PropertyGroup></Project>'
-    Out-File -InputObject $projectFileContents -FilePath $ToolsProj
-}
 
-dotnet add "$ToolsProj" package cake.coreclr -v "$CakeVersion" --package-directory "$CAKE_DIR"
+    New-Item -Path $ToolsDir -type directory | Out-Null
+    Out-File -Force -InputObject $projectFileContents -FilePath $ToolsProj
+
+    dotnet add "$ToolsProj" package cake.coreclr -v "$CakeVersion" --package-directory "$CAKE_DIR"
+}
 
 if (-not (Test-Path $CAKE_DLL))
 {
