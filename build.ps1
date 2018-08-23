881e59c4d50bf8952ddb445cdd8284d3895b2fb8
kimsagro1 <kimsagro@gmail.com>
2018-08-23 10:03:16 +1000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ Param
 (
     [String]$CakeVersion = "0.30.0",
     [String]$ToolPath    = [io.path]::combine($PSScriptRoot, "tools"),
-    [String]$ToolExe     = [io.path]::combine($ToolPath, "dotnet-cake.exe")
+    [String]$ToolExe     = [io.path]::combine($ToolPath, "dotnet-cake")
 )
 
 dotnet tool install --tool-path "$ToolPath" Cake.Tool --version $CakeVersion
